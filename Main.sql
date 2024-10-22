2e6f30b12744c3a17e89ed64a39c908eec259745
SirWirzd <gastorgas@uft.edu>
2024-10-21 22:00:38 -0300

    Falta procedimientos y triggers

--- Main.sql
+++ Main.sql
@@ -13,8 +13,8 @@ CREATE USER admin IDENTIFIED BY admin;
 GRANT ALL PRIVILEGES TO admin;
 
 -- ELiminación de tablas si existen previamente
-DROP TABLE PAISE CASCADE CONSTRAINTS;
-DROP TABLE CIUDADE CASCADE CONSTRAINTS;
+DROP TABLE PAIS CASCADE CONSTRAINTS;
+DROP TABLE CIUDAD CASCADE CONSTRAINTS;
 DROP TABLE AEROPUERTO CASCADE CONSTRAINTS;
 DROP TABLE VUELO CASCADE CONSTRAINTS;
 DROP TABLE PASAJERO CASCADE CONSTRAINTS;
@@ -27,21 +27,21 @@ DROP TABLE ASISTENCIA CASCADE CONSTRAINTS;
 
 CREATE TABLE PAIS(
     id_pais NUMBER NOT NULL,
-    nombre VARCHAR2(99) NOT NULL,
+    nombre VARCHAR2(100) NOT NULL,
     CONSTRAINT PK_PAIS PRIMARY KEY (id_pais)
 );
 
 CREATE TABLE CIUDAD(
     id_ciudad NUMBER NOT NULL,
-    nombre VARCHAR2(99) NOT NULL,
+    nombre VARCHAR2(100) NOT NULL,
     id_pais NUMBER NOT NULL,
-    CONSTRAINT PK_CIUDAD PRIMARY KEY (id_ciudad) ON DELETE CASCADE,
+    CONSTRAINT PK_CIUDAD PRIMARY KEY (id_ciudad),
     CONSTRAINT FK_CIUDAD_PAIS FOREIGN KEY (id_pais) REFERENCES PAIS(id_pais) ON DELETE CASCADE
 );
 
 CREATE TABLE AEROPUERTO(
     id_aeropuerto NUMBER NOT NULL,
-    nombre VARCHAR2(99) NOT NULL,
+    nombre VARCHAR2(100) NOT NULL,
     id_ciudad NUMBER NOT NULL,
     id_pais NUMBER NOT NULL,
     CONSTRAINT PK_AEROPUERTO PRIMARY KEY (id_aeropuerto),
@@ -51,17 +51,31 @@ CREATE TABLE AEROPUERTO(
 
 CREATE TABLE AEROLINEA(
     id_aerolinea NUMBER NOT NULL,
-    nombre_aerolinea VARCHAR2(99) NOT NULL,
+    nombre_aerolinea VARCHAR2(100) NOT NULL,
     CONSTRAINT PK_AEROLINEA PRIMARY KEY (id_aerolinea)
 );
 
+CREATE TABLE PASAJERO(
+    id_pasajero NUMBER NOT NULL,
+    nombre VARCHAR2(100) NOT NULL,
+    apellido VARCHAR2(100) NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    documento_identidad VARCHAR(20) NOT NULL,
+    CONSTRAINT PK_PASAJERO PRIMARY KEY (id_pasajero),
+    CONSTRAINT chck_documento_identidad CHECK (LENGTH(documento_identidad) <= 9),
+    CONSTRAINT chck_fecha_nacimiento CHECK (fecha_nacimiento < SYSDATE)
+);
+
 CREATE TABLE VUELO(
-    id_vuelo VARCHAR2(99) NOT NULL,
+    id_vuelo NUMBER NOT NULL,
     id_aerolinea_vuel NUMBER NOT NULL,
     id_aeropuerto_origen NUMBER NOT NULL,
     id_aeropuerto_destino NUMBER NOT NULL,
+    fecha_de_vuelo DATE NOT NULL,
     fecha_salida DATE NOT NULL,
     fecha_llegada DATE NOT NULL,
+    duracion NUMBER NOT NULL,
+    distancia NUMBER NOT NULL,
     estado VARCHAR2(20) CHECK (estado IN ('Programado', 'En vuelo', 'Aterrizado', 'Cancelado')),
     CONSTRAINT PK_VUELO PRIMARY KEY (id_vuelo),
     CONSTRAINT FK_VUELO_AEROLINEA FOREIGN KEY (id_aerolinea_vuel) REFERENCES AEROLINEA(id_aerolinea) ON DELETE CASCADE,
@@ -69,43 +83,45 @@ CREATE TABLE VUELO(
     CONSTRAINT FK_VUELO_AEROPUERTO_DESTINO FOREIGN KEY (id_aeropuerto_destino) REFERENCES AEROPUERTO(id_aeropuerto) ON DELETE CASCADE 
 );
 
-CREATE TABLE PASAJERO(
-    documento_identidad VARCHAR(99) NOT NULL,
-    nombre VARCHAR2(99) NOT NULL,
-    apellido VARCHAR2(99) NOT NULL,
-    fecha_nacimiento DATE NOT NULL,
-    edad NUMBER NOT NULL,
-    CONSTRAINT PK_PASAJERO PRIMARY KEY (documento_identidad)
+CREATE TABLE ASIENTO(
+    id_asiento NUMBER NOT NULL,
+    id_vuelo NUMBER NOT NULL,
+    numero_asiento VARCHAR2(10) NOT NULL,
+    estado VARCHAR2(20) CHECK (estado IN ('Disponible', 'No disponible')),
+    CONSTRAINT PK_ASIENTO PRIMARY KEY (id_asiento),
+    CONSTRAINT FK_ASIENTO_VUELO FOREIGN KEY (id_vuelo) REFERENCES VUELO(id_vuelo) ON DELETE CASCADE
 );
 
 CREATE TABLE COMPRA(
     id_compra NUMBER NOT NULL,
     id_pasajero_comp NUMBER NOT NULL,
-    id_vuelo_comp VARCHAR2(99) NOT NULL,
+    id_vuelo_comp NUMBER NOT NULL,
     fecha_compra DATE NOT NULL,
     monto_total_compra NUMBER NOT NULL,
-    motivo_viaje VARCHAR2(99) NOT NULL,
+    motivo_viaje VARCHAR2(100) NOT NULL,
     CONSTRAINT PK_COMPRA PRIMARY KEY (id_compra),
-    CONSTRAINT FK_COMPRA_PASAJERO FOREIGN KEY (id_pasajero_comp) REFERENCES PASAJERO(documento_identidad) ON DELETE CASCADE,
+    CONSTRAINT FK_COMPRA_PASAJERO FOREIGN KEY (id_pasajero_comp) REFERENCES PASAJERO(id_pasajero) ON DELETE CASCADE,
     CONSTRAINT FK_COMPRA_VUELO FOREIGN KEY (id_vuelo_comp) REFERENCES VUELO(id_vuelo) ON DELETE CASCADE
 );
 
 CREATE TABLE RESERVA(
     id_reserva NUMBER NOT NULL,
     id_pasajero_res NUMBER NOT NULL,
-    id_vuelo_res VARCHAR2(99) NOT NULL,
+    id_vuelo_res NUMBER NOT NULL,
+    id_asiento NUMBER NOT NULL,
     fecha_reserva DATE NOT NULL,
     CONSTRAINT PK_RESERVA PRIMARY KEY (id_reserva),
-    CONSTRAINT FK_RESERVA_PASAJERO FOREIGN KEY (id_pasajero_res) REFERENCES PASAJERO(documento_identidad) ON DELETE CASCADE,
-    CONSTRAINT FK_RESERVA_VUELO FOREIGN KEY (id_vuelo_res) REFERENCES VUELO(id_vuelo) ON DELETE CASCADE
+    CONSTRAINT FK_RESERVA_PASAJERO FOREIGN KEY (id_pasajero_res) REFERENCES PASAJERO(id_pasajero) ON DELETE CASCADE,
+    CONSTRAINT FK_RESERVA_VUELO FOREIGN KEY (id_vuelo_res) REFERENCES VUELO(id_vuelo) ON DELETE CASCADE,
+    CONSTRAINT FK_RESERVA_ASIENTO FOREIGN KEY (id_asiento) REFERENCES ASIENTO(id_asiento) ON DELETE CASCADE
 );
 
 CREATE TABLE EQUIPAJE(
     id_equipaje NUMBER NOT NULL,
     id_pasajero_eq NUMBER NOT NULL,
-    id_vuelo_eq VARCHAR2(99) NOT NULL,
+    id_vuelo_eq NUMBER NOT NULL,
     peso NUMBER NOT NULL,
-    descripcion VARCHAR2(99) NOT NULL,
+    descripcion VARCHAR2(100) NOT NULL,
     CONSTRAINT PK_EQUIPAJE PRIMARY KEY (id_equipaje),
     CONSTRAINT FK_EQUIPAJE_PASAJERO FOREIGN KEY (id_pasajero_eq) REFERENCES PASAJERO(documento_identidad) ON DELETE CASCADE,
     CONSTRAINT FK_EQUIPAJE_VUELO FOREIGN KEY (id_vuelo_eq) REFERENCES VUELO(id_vuelo) ON DELETE CASCADE
@@ -113,11 +129,18 @@ CREATE TABLE EQUIPAJE(
 
 CREATE TABLE SERVICIO(
     id_servicio NUMBER NOT NULL,
+    nombre_servicio VARCHAR2(100) NOT NULL,
+    costo NUMBER NOT NULL,
+    CONSTRAINT PK_SERVICIO PRIMARY KEY (id_servicio)
+);
+
+CREATE TABLE SERVICIOS_PASAJERO(
+    id_servicio_pasajero NUMBER NOT NULL,
     id_pasajero_ser NUMBER NOT NULL,
-    nombre_servicio VARCHAR2(99) NOT NULL,
+    nombre_servicio VARCHAR2(100) NOT NULL,
     precio_servicio NUMBER NOT NULL,
-    CONSTRAINT PK_SERVICIOS PRIMARY KEY (id_servicio),
-    CONSTRAINT FK_SERVICIOS_PASAJERO FOREIGN KEY (id_pasajero_ser) REFERENCES PASAJERO(documento_identidad) ON DELETE CASCADE
+    CONSTRAINT PK_SERVICIOS PRIMARY KEY (id_servicio_pasajero),
+    CONSTRAINT FK_SERVICIOS_PASAJERO FOREIGN KEY (id_pasajero_ser) REFERENCES PASAJERO(id_pasajero) ON DELETE CASCADE
 );
 
 CREATE TABLE ASISTENCIA(
@@ -125,15 +148,29 @@ CREATE TABLE ASISTENCIA(
     id_pasajero_asi NUMBER NOT NULL,
     tipo_asistencia VARCHAR2(99) NOT NULL,
     CONSTRAINT PK_ASISTENCIA PRIMARY KEY (id_asistencia),
-    CONSTRAINT FK_ASISTENCIA_PASAJERO FOREIGN KEY (id_pasajero_asi) REFERENCES PASAJERO(documento_identidad) ON DELETE CASCADE,
-    CONSTRAINT chck_tipo_asistencia CHECK(tipo_asistencia IN ('Discapacidad', 'Embarazo', 'Tercera Edad', 'Niños')) ON DELETE CASCADE
+    CONSTRAINT FK_ASISTENCIA_PASAJERO FOREIGN KEY (id_pasajero_asi) REFERENCES PASAJERO(id_pasajero) ON DELETE CASCADE,
+    CONSTRAINT chck_tipo_asistencia CHECK(tipo_asistencia IN ('Discapacidad', 'Embarazo', 'Tercera Edad', 'Niños'))
 );
+
+-- Cambiar formato de la fecha
+
+ALTER SESSION SET NLS_DATE_FORMAT = 'DD-MM-YYYY';
 
 -- Secuencias para generar los IDs automáticos
 
+CREATE SEQUENCE seq_pais START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE seq_ciudad START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE seq_aeropuerto START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE seq_aerolinea START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE seq_pasajero START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE seq_vuelo START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE seq_asiento START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE seq_compra START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE seq_reserva START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE seq_servicios_pasajeros START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE seq_equipaje START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE seq_servicio START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE seq_servicios_pasajero START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE seq_asistencia START WITH 1 INCREMENT BY 1;
 
 -- Trigger para validad si un asiento está disponible
 
@@ -148,7 +185,7 @@ BEGIN
     FROM RESERVA
     WHERE id_vuelo = :NEW.id_vuelo 
     AND id_asiento = :NEW.id_asiento
-    AND estado = "Confirmado";
+    AND estado = 'Confirmado';
 
     IF v_asiento_ocupado > 0 THEN
         RAISE_APPLICATION_ERROR(-20001, 'Asiento ya está ocupado');
@@ -161,10 +198,65 @@ CREATE OR REPLACE TRIGGER ACTUALIZAR_ESTADO_VUELO
 AFTER INSERT OR UPDATE ON VUELO
 FOR EACH ROW
 BEGIN
-    IF (:NEW.fecha_salida < SYSDATE) THEN
+    IF (:NEW.fecha_salida < SYSDATE AND :NEW.fecha_llegada > SYSDATE) THEN
         UPDATE VUELO
         SET estado = 'En vuelo' 
         WHERE id_vuelo = :NEW.id_vuelo;
+    ELSIF (:NEW.fecha_llegada <= SYSDATE) THEN
+        UPDATE VUELO
+        SET estado = 'Aterrizado' 
+        WHERE id_vuelo = :NEW.id_vuelo;
+    END IF;
+END;
+
+-- Trigger para actualizar el estado de compra
+
+CREATE OR REPLACE TRIGGER ACTUALIZAR_ESTADO_COMPRA
+AFTER INSERT OR UPDATE ON COMPRA
+FOR EACH ROW
+BEGIN
+    IF (:NEW.fecha_compra < SYSDATE) THEN
+        UPDATE COMPRA
+        SET estado = 'Confirmado' 
+        WHERE id_compra = :NEW.id_compra;
+    END IF;
+END;
+
+-- Trigger para actualizar el estado de asiento
+
+CREATE OR REPLACE TRIGGER ACTUALIZAR_ESTADO_ASIENTO
+AFTER INSERT OR UPDATE ON ASIENTO
+FOR EACH ROW
+BEGIN
+    IF (:NEW.estado = 'Reservado') THEN
+        UPDATE ASIENTO
+        SET estado = 'No disponible' 
+        WHERE id_asiento = :NEW.id_asiento;
+    END IF;
+END;
+
+-- Trigger para actualizar el estado de asistencia
+
+CREATE OR REPLACE TRIGGER ACTUALIZAR_ESTADO_ASISTENCIA
+AFTER INSERT OR UPDATE ON ASISTENCIA
+FOR EACH ROW
+BEGIN
+    IF (:NEW.tipo_asistencia = 'Discapacidad') THEN
+        UPDATE ASISTENCIA
+        SET estado = 'Discapacidad' 
+        WHERE id_asistencia = :NEW.id_asistencia;
+    ELSIF (:NEW.tipo_asistencia = 'Embarazo') THEN
+        UPDATE ASISTENCIA
+        SET estado = 'Embarazo' 
+        WHERE id_asistencia = :NEW.id_asistencia;
+    ELSIF (:NEW.tipo_asistencia = 'Tercera Edad') THEN
+        UPDATE ASISTENCIA
+        SET estado = 'Tercera Edad' 
+        WHERE id_asistencia = :NEW.id_asistencia;
+    ELSIF (:NEW.tipo_asistencia = 'Niños') THEN
+        UPDATE ASISTENCIA
+        SET estado = 'Niños' 
+        WHERE id_asistencia = :NEW.id_asistencia;
     END IF;
 END;
 
